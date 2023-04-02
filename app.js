bc8c5235517a55da0427dded7f1b70ad2d697870
DESKTOP-NOMKETR\adnan <11adnan@live.com>
2023-04-02 19:56:59 +0600

    fix deploy issue

--- app.js
+++ app.js
@@ -41,22 +41,21 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
+//app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(cors());
 app.use(express.static("./public"));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.send("e commercve api");
-});
+// app.get("/", (req, res) => {
+//   res.send("e commercve api");
+// });
 
-app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies);
-  //console.log(req.cookies);
-  res.send("e commercve api");
-});
+// app.get("/api/v1", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.send("e commercve api");
+// });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
